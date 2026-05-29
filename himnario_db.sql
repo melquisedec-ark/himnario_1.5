fa29aa531c77ecd17da9c4470615a994619a8eb8
Melqui <melquisedec.ark@gmail.com>
2026-05-29 10:43:44 -0600

    fix: restore bcrypt password hash for admin login (was overwritten by SQLite migration)

--- himnario_db.sql
+++ himnario_db.sql
@@ -353,7 +353,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'admin','240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9','Administrador','Admin','2026-05-14 19:02:52');
+INSERT INTO `usuarios` VALUES (1,'admin','$2y$12$j5A9zvEbX19lmTt2eBmlRucaAVoPrxKXY3nrwh62oV5ACirzDEyce','Administrador','Admin','2026-05-14 19:02:52');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -398,4 +398,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-29 16:35:04
+-- Dump completed on 2026-05-29 16:43:29
